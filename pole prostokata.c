15aacb4ad3c2245eace9f152dac8ead9c3145f1e
Michal Joozefiak <michal.jozefiak24@gmail.com>
2021-04-20 12:52:55 +0200

    Dodano kod oraz menu

--- pole prostokata.c
+++ pole prostokata.c
@@ -2,21 +2,53 @@
 #include <stdlib.h>
 #include <math.h>
 
-int main(){
-
-    float a;
-    float b;
-    float pole_prostokata;
-
-    printf ("podaj dlugosc boku a: ");
-    scanf ("%f", &a);
-
-    printf ("podaj dlugosc boku b: ");
-    scanf ("%f", &b);
 
 
-    pole_prostokata = a * b;
-    printf ("pole_prostokata: %f", pole_prostokata);
+int main()
+{
+for(;;)
+{
+int c=68 ;
+float a, b, h, wynik, pods, wynik2;
 
+    printf("wybierz operacje\n");
+    printf("1.pole prostokata\n");
+    printf("2.pole trojkata\n");
+    printf("3. wyjscie\n");
+    scanf("%d", &c);
+switch (c)
+
+
+{
+
+case 1:
+
+    printf("wprowadz dlugosc boku a\n");
+    scanf("%f", &a);
+    printf("wprowadz dlugosc boku b\n");
+    scanf("%f", &b);
+    wynik = a*b ;
+    printf("wynik=" "%f\n", wynik);
+
+    break;
+case 2:
+
+    printf("wprowadz dlugosc podstawy\n");
+    scanf("%f", &pods);
+    printf("pods=" , pods);
+    printf("wprowadz dlugosc wysokosci");
+    scanf("%f", &h);
+    wynik2 = pods*h / 2 ;
+    printf("wynik=" "%f\n", wynik2);
+    break;
+case 3:
+   return 0;
+default:
+  printf("Zly wybor\n");
+}
+
+
+}
     return 0;
+
 }
